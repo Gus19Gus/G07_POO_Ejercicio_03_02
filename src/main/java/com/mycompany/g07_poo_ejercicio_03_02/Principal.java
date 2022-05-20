7feedc85fed49cf7821efc20db284ca549e8148d
HP USER <HP USER@LAPTOP-6UPDUBH9>
2022-05-20 07:37:36 -0500

    Creacion Ventena 3

---
+++ src/main/java/com/mycompany/g07_poo_ejercicio_03_02/Principal.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.g07_poo_ejercicio_03_02;
+
+/**
+ *
+ * @author Gus-Gus
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+       var v1 = new Ventana1("INSCRIBETE");
+       v1.setVisible(true);
+       var v2 = new Ventana2("DATOS PERSONALES");
+       v2.setVisible(true);
+       var v3 = new Ventana3("INFORMACION DE CARRERA");
+       v3.setVisible(true);
+    }
+}
---
+++ src/main/java/com/mycompany/g07_poo_ejercicio_03_02/Ventana4.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.g07_poo_ejercicio_03_02;
+
+/**
+ *
+ * @author Gus-Gus
+ */
+public class Ventana4 {
+
+}
